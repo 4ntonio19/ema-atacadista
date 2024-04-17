2295314289365bbb9a70ccaefc416e7f4c6ef2da
4ntonio19 <vgoncalvescarvalho@outlook.com>
2024-04-17 01:06:34 -0300

    feat/contacts component

--- js/scripts.js
+++ js/scripts.js
@@ -10,45 +10,30 @@ menuButton.addEventListener("click", () => {
   header.style.height = headerHeight + "px";
 });
 
-// let index = 0;
-// const carouselContent = document.querySelector(".section-parceiros-carrousel");
-// const cards = document.querySelectorAll(".card");
+let swiper = new Swiper(".swiper-container", {
+  loop: true, // Ativa o loop contínuo
+  slidesPerView: 4, // Exibe 5 slides por vez
+  spaceBetween: 20, // Espaçamento entre os slides
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
 
-// function showSlide(i) {
-//   index = i;
-//   if (index < 0) {
-//     index = cards.length - 1;
-//   } else if (index >= cards.length) {
-//     index = 0;
-//   }
-//   const cardWidth = cards[0].offsetWidth;
-//   const offset =
-//     -index * (cardWidth + parseInt(getComputedStyle(cards[0]).marginRight));
-//   carouselContent.style.transform = `translateX(${offset}px)`;
-// }
-
-// function nextSlide() {
-//   showSlide(index + 1);
-// }
-
-// function prevSlide() {
-//   showSlide(index - 1);
-// }
-
-// setInterval(nextSlide, 5000);
-
-// Inicialize o carrossel Swiper com configurações de loop contínuo
-// import Swiper from "swiper";
-
-// const swiperCarousel = new Swiper(".swiper-container", {
-//   loop: true, // Ativa o loop contínuo
-//   slidesPerView: 5, // Exibe 5 slides por vez
-//   spaceBetween: 20, // Espaçamento entre os slides
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   autoplay: {
-//     delay: 5000,
-//   },
-// });
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    662: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
